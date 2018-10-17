1d3b8637d43d8d1fb4f6d1f6200b675cfb03423f
Luke Helg <luke@exolever.com>
2018-10-17 16:23:53 +0900

    Update blot create method to return all dataset values

--- src/blots/mention.js
+++ src/blots/mention.js
@@ -11,18 +11,19 @@ class MentionBlot extends Embed {
     denotationChar.innerHTML = data.denotationChar;
     node.appendChild(denotationChar);
     node.innerHTML += data.value;
-    node.dataset.id = data.id;
-    node.dataset.value = data.value;
-    node.dataset.denotationChar = data.denotationChar;
-    return node;
+    return MentionBlot.setDataValues(node, data);
+  }
+
+  static setDataValues(element, data) {
+    const domNode = element;
+    Object.keys(data).forEach((key) => {
+      domNode.dataset[key] = data[key];
+    });
+    return domNode;
   }
 
   static value(domNode) {
-    return {
-      id: domNode.dataset.id,
-      value: domNode.dataset.value,
-      denotationChar: domNode.dataset.denotationChar,
-    };
+    return domNode.dataset;
   }
 }
 
