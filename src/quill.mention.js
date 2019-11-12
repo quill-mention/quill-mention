2b796c4e69d5c7fb64d5861f25e6a5b37e10095c
Jürgen <jurgenvisser@Davids-MBP.home>
2019-11-12 12:57:25 +0100

    fixed keycodes to work with quill 2

--- src/quill.mention.js
+++ src/quill.mention.js
@@ -76,12 +76,12 @@ class Mention {
     quill.keyboard.addBinding({
       key: Keys.TAB,
     }, this.selectHandler.bind(this));
-    quill.keyboard.bindings[9].unshift(quill.keyboard.bindings[9].pop());
+    quill.keyboard.bindings[Keys.TAB].unshift(quill.keyboard.bindings[Keys.TAB].pop());
 
     quill.keyboard.addBinding({
       key: Keys.ENTER,
     }, this.selectHandler.bind(this));
-    quill.keyboard.bindings[13].unshift(quill.keyboard.bindings[13].pop());
+    quill.keyboard.bindings[Keys.ENTER].unshift(quill.keyboard.bindings[Keys.ENTER].pop());
 
     quill.keyboard.addBinding({
       key: Keys.ESCAPE,
