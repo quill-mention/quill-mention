855e6b6b775a6c55abd166af19078712ece83276
Fredrik Sundqvist <fsundqvist@gmail.com>
2019-12-05 10:43:12 +0100

    Add minified iife output

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,32 @@
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
 
 
 export default [
   {
     input: 'src/quill.mention.js',
-    external: ['quill'],
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
+      {
+        file: pkg.main,
+        format: 'cjs',
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+      },
+      {
+        file: 'dist/quill.mention.min.js',
+        format: 'iife',
+        name: 'quillMention',
+        plugins: [terser()],
+        globals: {
+          quill: 'Quill',
+        },
+      },
     ],
+    external: ['quill'],
     plugins: [
       babel({
         exclude: ['node_modules/**'],
