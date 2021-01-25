97ad87b946dfbbf24ab5fb08dddd582cf7a8fd37
lvillap <lvillap@gmail.com>
2021-01-26 00:54:13 +0100

    Signal sent when mention is clicked
    Allows any client code to receive a 'mention-clicked' signal when a mention is clicked. Useful to do some kind of navigation, show an info popup, etc

--- src/blots/mention.js
+++ src/blots/mention.js
@@ -5,6 +5,14 @@ const Embed = Quill.import("blots/embed");
 class MentionBlot extends Embed {
   static create(data) {
     const node = super.create();
+    /* When clicked, sends a signal to be catched by any client code */
+    node.addEventListener('click', function(ev) {
+        const event = document.createEvent("Event");
+        event.initEvent("mention-clicked", true, true);
+        event.value = data;
+        window.dispatchEvent(event);
+        ev.preventDefault();
+      }, false);
     const denotationChar = document.createElement("span");
     denotationChar.className = "ql-mention-denotation-char";
     denotationChar.innerHTML = data.denotationChar;
