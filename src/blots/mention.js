45a8aff6a419027c634974e7ef10f1f7b28666f1
lvillap <lvillap@gmail.com>
2021-01-26 01:10:46 +0100

    Singal when mention is clicked
    When a mention is clicked, a signal is sent, so it can be captured and applications can react to it

--- src/blots/mention.js
+++ src/blots/mention.js
@@ -5,6 +5,15 @@ const Embed = Quill.import("blots/embed");
 class MentionBlot extends Embed {
   static create(data) {
     const node = super.create();
+    /* Sends a signal each time the node is clicked, so it can be listened to */
+    node.addEventListener('click', function(ev) {
+        const event = document.createEvent("Event");
+        event.initEvent("mention-clicked", true, true);
+        event.value = data;
+        event.clickEvent = ev;
+        window.dispatchEvent(event);
+        ev.preventDefault();
+      }, false); 
     const denotationChar = document.createElement("span");
     denotationChar.className = "ql-mention-denotation-char";
     denotationChar.innerHTML = data.denotationChar;
