162aa0b324edf403a57deab9cc21e0fbbd5d5691
keiji <limbresidence@gmail.com>
2021-04-07 22:31:49 +0300

    feat: mention-click

--- src/blots/mention.js
+++ src/blots/mention.js
@@ -5,6 +5,13 @@ const Embed = Quill.import("blots/embed");
 class MentionBlot extends Embed {
   static create(data) {
     const node = super.create();
+    node.addEventListener('click', (e) => {
+      const event = new Event('mention-clicked', {bubbles: true, cancelable: true});
+      event.value = data;
+      event.event = e;
+      window.dispatchEvent(event);
+      e.preventDefault();
+    }, false);
     const denotationChar = document.createElement("span");
     denotationChar.className = "ql-mention-denotation-char";
     denotationChar.innerHTML = data.denotationChar;
