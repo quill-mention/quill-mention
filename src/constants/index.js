ab9d2f99df558fe40e975352515e65d02182b7f8
dan-bartlettroofs <dan@bartlettroofs.com>
2024-04-30 08:58:03 -0600

    linting run

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
 const Keys = {
-	TAB: 'Tab',
-	ENTER: 'Enter',
-	ESCAPE: 'Escape',
-	UP: 'ArrowUp',
-	DOWN: 'ArrowDown'
+  TAB: "Tab",
+  ENTER: "Enter",
+  ESCAPE: "Escape",
+  UP: "ArrowUp",
+  DOWN: "ArrowDown",
 };
 
 export default Keys;
