1fd7cef4ff7c09fa3ceb5f93536137205096bbbb
Fredrik Sundqvist <3159565+MadSpindel@users.noreply.github.com>
2024-05-03 17:07:28 +0200

    Revert "Fixes for Quill 2.0"

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
 const Keys = {
-	TAB: 'Tab',
-	ENTER: 'Enter',
-	ESCAPE: 'Escape',
-	UP: 'ArrowUp',
-	DOWN: 'ArrowDown'
+  TAB: 9,
+  ENTER: 13,
+  ESCAPE: 27,
+  UP: 38,
+  DOWN: 40,
 };
 
 export default Keys;
